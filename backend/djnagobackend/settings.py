fad3664ec1f3fd68d660882fd1553e1f74cb4967
Varchasavkr <Varchasav25@gmail.com>
2024-07-16 10:20:45 +0530

    Update settings.py

--- backend/djnagobackend/settings.py
+++ backend/djnagobackend/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'mat2pred-backend.onrender.com'
+]
 
 
 # Application definition
